343f3f8901522023fdce062b562ccdb9e6e77960
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-09-27 15:24:16 -0600

    adds number to DOM

--- script.js
+++ script.js
@@ -10,46 +10,46 @@ function writePassword() {
   var charSpec = confirm("Would you like to include special characters in your password?");
 
   generatePassword();
-  
+
   //Function to take parameters from alerts and create a password
   function generatePassword() {
     var alphaLower = "abcdefghijklmnopqrstuvwxyz";
     var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var nums = "0123456789";
     var chars = "!@#$%^&*_+~`|\:;?><,./-=";
-    var codeSpec = ""
-    
-      if (charCase === true && charNum === true && charSpec === true){
-        codeSpec = alphaLower + alphaUpper + nums + chars
-      } else if (charCase === false && charNum === true && charSpec === true){
-        codeSpec = alphaLower + nums + chars
-      } else if (charCase === false && charNum === false && charSpec === true){
-        codeSpec = alphaLower + chars
-      } else if (charCase === false && charNum === false && charSpec === false){
-        codeSpec = alphaLower
-      } else if (charCase === true && charNum === false && charSpec === false){
-        codeSpec = alphaLower + alphaUpper
-      }  else if (charCase === true && charNum === true && charSpec === false){
-        codeSpec = alphaLower + alphaUpper + nums
-      }  else if (charCase === true && charNum === false && charSpec === true){
-        codeSpec = alphaLower + alphaUpper + chars
-      }else {
-        codeSpec = "There is an error"
-      }
-      console.log("These are your code options " + codeSpec )
+    var codeSpec = "";
 
-      var shuffled = codeSpec.split('').sort(function(){return 0.5-Math.random()}).join('');
+    if (charCase === true && charNum === true && charSpec === true) {
+      codeSpec = alphaLower + alphaUpper + nums + chars
+    } else if (charCase === false && charNum === true && charSpec === true) {
+      codeSpec = alphaLower + nums + chars
+    } else if (charCase === false && charNum === false && charSpec === true) {
+      codeSpec = alphaLower + chars
+    } else if (charCase === false && charNum === false && charSpec === false) {
+      codeSpec = alphaLower
+    } else if (charCase === true && charNum === false && charSpec === false) {
+      codeSpec = alphaLower + alphaUpper
+    } else if (charCase === true && charNum === true && charSpec === false) {
+      codeSpec = alphaLower + alphaUpper + nums
+    } else if (charCase === true && charNum === false && charSpec === true) {
+      codeSpec = alphaLower + alphaUpper + chars
+    } else {
+      codeSpec = "There is an error"
+    }
+    console.log("These are your code options " + codeSpec);
+    console.log(codeSpec.length);
 
-      var finalPass = shuffled.substring(0, numLength);
+    var shuffled = codeSpec.split('').sort(function () { return 0.5 - Math.random() }).join('');
 
-      console.log(finalPass)
+    var finalPass = shuffled.substring(0, numLength);
+
+    console.log(finalPass);
+
+    // which div to put the password into.
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = finalPass;
   }
-
-  // which div to put the password into.
-  // var passwordText = document.querySelector("#password");
-  
-  // passwordText.value = password;
-
 }
 
 // Add event listener to generate button
