f539a77304b59cb5df7fb8bf560d2d4e997c92f7
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-10-01 18:41:05 -0600

    cleaned up files

--- script.js
+++ script.js
@@ -1,61 +1,52 @@
-// Assignment Code
 //Which button to assign to generateBtn
 var generateBtn = document.querySelector("#generate");
+//Var for final password
+var finalPass = "";
 
 // Ask how long and which characters will be in the password
 function generatePassword() {
-  var numLength = prompt("How many characters would you like the password to be?");
-  if (numLength < 8 || numLength > 128){
+  var numLength = prompt("How many characters (8-128) would you like the password to be?");
+  if (numLength < 8 || numLength > 128) {
     alert("Please enter a number between 8 - 128")
     generatePassword()
   } else {
-  var charCase = confirm("Would you like to include uppercase letters in your password?");
-  var charNum = confirm("Would you like to include numeric characters in your password?");
-  var charSpec = confirm("Would you like to include special characters in your password?");
+    var charCase = confirm("Would you like to include uppercase letters in your password?");
+    var charNum = confirm("Would you like to include numeric characters in your password?");
+    var charSpec = confirm("Would you like to include special characters in your password?");
 
-  writePassword();
+    writePassword();
   }
   //Function to take parameters from alerts and create a password then print it to the dom
   function writePassword() {
-    var alphaLower = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-    var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var nums = "01234567890123456789";
-    var chars = "!@#$%^&*_+~`|\:;?><,./-=!@#$%^&*_+~`|\:;?><,./-=";
-    var codeSpec = "";
+    var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+    var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var nums = "0123456789";
+    var chars = "!@#$%^&*_+~`|\:;?><,./-=";
+    var codeSpec = alphaLower;
 
-    if (charCase === true && charNum === true && charSpec === true) {
-      codeSpec = alphaLower + alphaUpper + nums + chars
-    } else if (charCase === false && charNum === true && charSpec === true) {
-      codeSpec = alphaLower + nums + chars
-    } else if (charCase === false && charNum === false && charSpec === true) {
-      codeSpec = alphaLower + chars
-    } else if (charCase === false && charNum === false && charSpec === false) {
-      codeSpec = alphaLower
-    } else if (charCase === true && charNum === false && charSpec === false) {
-      codeSpec = alphaLower + alphaUpper
-    } else if (charCase === true && charNum === true && charSpec === false) {
-      codeSpec = alphaLower + alphaUpper + nums
-    } else if (charCase === true && charNum === false && charSpec === true) {
-      codeSpec = alphaLower + alphaUpper + chars
-    } else {
-      codeSpec = "There is an error"
+    if (charCase) {
+      codeSpec = codeSpec + alphaUpper
+    } if (charNum) {
+      codeSpec = codeSpec + nums
+    } if (charSpec) {
+      codeSpec = codeSpec + chars
     }
+
     console.log("These are your code options " + codeSpec);
     console.log(codeSpec.length);
+    console.log(numLength);
+    for (var i = numLength; i > 0; i--) {
+      // i is the number of characters and keep running the loop until that number is decremented to 0
+      // I need to loop through the codeSpecArray and randomly pick a character to add to finalPass
+      finalPass = finalPass + codeSpec[Math.floor(Math.random() * codeSpec.length-1)];
 
-    var shuffled = codeSpec.split('').sort(function () { return 0.5 - Math.random() }).join('');
-
-    var finalPass = shuffled.substring(0, numLength);
-
-    console.log(finalPass);
-
-    // which div to put the password into.
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = finalPass;
+    }
   }
-}
+  // which div to put the password into.
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = finalPass;
+}
 // Add event listener to generate button
 // Code to activate writePassword function on click
 generateBtn.addEventListener("click", generatePassword);
